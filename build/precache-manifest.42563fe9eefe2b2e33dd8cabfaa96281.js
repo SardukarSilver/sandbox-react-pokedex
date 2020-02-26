f4af3822d716af5ede864ff52c0cf8b877590476
Nikita Chotiy <nikita.chotiy@eosda.com>
2020-02-26 11:34:03 +0200

    minor changes

---
+++ build/precache-manifest.42563fe9eefe2b2e33dd8cabfaa96281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb91c0ef7747fcf4e31e20204d4735a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d11429e2ff10c715fd8b",
+    "url": "/static/css/main.892dd89d.chunk.css"
+  },
+  {
+    "revision": "d0a942c0fecd970b2ef6",
+    "url": "/static/js/2.8f735249.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8f735249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d11429e2ff10c715fd8b",
+    "url": "/static/js/main.9c4eb295.chunk.js"
+  },
+  {
+    "revision": "a80d7b10b7a783811a41",
+    "url": "/static/js/runtime-main.38e97746.js"
+  }
+]);
