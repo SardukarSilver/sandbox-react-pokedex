362100bb677365c60b8bd817facaddf0a847a37b
Nikita Chotiy <nikita.chotiy@eosda.com>
2020-02-26 11:41:30 +0200

    Updates

---
+++ precache-manifest.5669c2ac3d77d360fa883c2135c959bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e39385624d4f92c87c36cd31aada34b",
+    "url": "/sandbox-react-pokedex/index.html"
+  },
+  {
+    "revision": "e6dc8d7a9a8435473dc2",
+    "url": "/sandbox-react-pokedex/static/css/main.892dd89d.chunk.css"
+  },
+  {
+    "revision": "afad2a3aff793d7fc064",
+    "url": "/sandbox-react-pokedex/static/js/2.217c2afa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sandbox-react-pokedex/static/js/2.217c2afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6dc8d7a9a8435473dc2",
+    "url": "/sandbox-react-pokedex/static/js/main.de8aaaa0.chunk.js"
+  },
+  {
+    "revision": "e40e0b431a6b813568bb",
+    "url": "/sandbox-react-pokedex/static/js/runtime-main.56ef3b02.js"
+  }
+]);
